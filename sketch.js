f57b848fb1ecb9ae681e468bb31580e2f8526f98
J-bott <88146840+J-bott@users.noreply.github.com>
2024-05-23 17:06:02 +0100

    fps optimization/sonar fix

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-let num =1500;
+let num = 750;
 let linkedNum = 14;
 let noiseScale = 500;
 let noiseStrength = 2;
@@ -15,53 +15,58 @@ function setup() {
 
   createCanvas(windowWidth*2, windowHeight*2);
   noStroke();
+
   extraCanvas = createGraphics(windowWidth*2, windowHeight*2); // Set the same size as the main canvas
-  extraCanvas.blendMode(MULTIPLY); // Set blend mode to multiply
 
   // normal particles
   for (let i = 0; i < num; i++) {
-    var loc = createVector(random(width * 1.2), random(height), 2);
+    var loc = createVector(random(width*0.15, width*0.85), random(height*0.15, height*0.85));
     var angle = 0;
     var dir = createVector(cos(angle), sin(angle));
     var speed = random(0.5, 2);
-    particles[i] = new Particle(loc, dir, speed,200);
+    particles[i] = new Particle(loc, dir, speed, 200);
   }
 
-    // special particles
-    for (let i = 0; i < linkedNum; i++) {
-      var loc = createVector(random(width * 1.2), random(height), 2);
-      var angle = 0;
-      var dir = createVector(cos(angle), sin(angle));
-      var speed = random(0.5, 2);
-      particles[i] = new Particle(loc, dir, speed, 201);
+  // special particles
+  for (let i = 0; i < linkedNum; i++) {
+    var loc = createVector(random(width*0.15, width*0.85), random(height*0.25, height*0.75));
+    var angle = 0;
+    var dir = createVector(cos(angle), sin(angle));
+    var speed = random(0.5, 2);
+    particles[i] = new Particle(loc, dir, speed, 201);
   }
 }
 
 function windowResized() {
-  resizeCanvas(800, 600);
+  resizeCanvas(windowWidth*2, windowHeight*2);
 }
 
 function draw() {
+  background(5);
 
-  fill(5, 5);
-  rect(0, 0, width, height);
+  extraCanvas.background(5, 5);
 
   for (let i = 0; i < particles.length; i++) {
     particles[i].run();
-    particles[i].sonarHit();
-  }
-    // Clear the extra canvas
-    
-    // Display all circles
-    for (let i = 0; i < pulses.length; i++) {
-      let pulse = pulses[i];
-      pulse.display();
-      pulse.update();
-      image(extraCanvas, 0, 0);  
-      extraCanvas.clear();    
+    if (particles[i].color != 200) {
+      particles[i].sonarHit();
     }
     
-    // Draw the extra canvas onto the main canvas
+  }
+
+  image(extraCanvas, 0, 0); 
+  // Clear the extra canvas
+    
+  // Display all circles
+  for (let i = 0; i < pulses.length; i++) {
+    let pulse = pulses[i];
+    pulse.update();
+    pulse.display();
+  }
+
+  
+    
+  // Draw the extra canvas onto the main canvas
     
 }
 
